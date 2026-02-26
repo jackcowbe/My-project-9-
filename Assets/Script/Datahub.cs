20b5327b4c005c0054ec6c1304605691e9d8a91b
jackcowbe <jackcowbe@gmail.com>
2026-02-26 18:55:48 +0800

    Initial commit

---
+++ Assets/Script/Datahub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+// Span<int> sliceX = x.AsSpan().Slice(int,int); // 創建一個切片來訪問X座標的一部分
+public class Datahub 
+{
+    public const float squareSizew = 1.1f;  //寬度
+    public const float squareSizeh = 1.1f;  //高度
+    public const int z = 10; // 相機高度
+    static public readonly int[] x = new int[10000]; // 10000个格子，每个格子一个x坐标
+    static public readonly int[] y = new int[10000]; // 10000个格子，每个格子一个y坐标
+   
+
+    static public int[] mouseX = new int[1]; // 鼠标点击位置的X坐标
+    static public int[] mouseY = new int[1]; // 鼠标点击位置的Y坐标
+    
+
+
+
+}
+
+
+                    
---
+++ Assets/Script/Gird.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SqlTypes;
+using UnityEngine;
+
+public class Gird : MonoBehaviour
+{
+    
+    public GameObject square;
+    private int n = 0;
+
+    static public Action<GameObject,int,int> ready;
+    static public Action<GameObject> OK;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i<100; i++)
+        {
+            for (int j = 0; j<100; j++)
+            {
+                Instantiate(square,new Vector3(i*Datahub.squareSizew,j*Datahub.squareSizeh,0),Quaternion.identity);
+                ready?.Invoke(square,i,j);
+                OK?.Invoke(square);
+                Datahub.x[n] = i;
+                Datahub.y[n] = j;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private class  coordinate:MonoBehaviour
+    {
+        public int row = 0, column = 0;
+        public int[,] gird = new int[100, 100];
+        
+        private void Start()
+        {
+             ready += coorsystem;
+        }
+
+        public void coorsystem(GameObject square,int row, int column)
+        {
+            
+            this.row = row;
+            this.column = column;
+        }
+    }
+}
