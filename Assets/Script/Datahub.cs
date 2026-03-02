7609843b60b85909369801c9d9202cfb247231b5
jackcowbe <jackcowbe@gmail.com>
2026-03-02 21:39:43 +0800

    DOD 數據導向架構與 Job System 初始化
    感覺好像有點感覺有點爽

--- Assets/Script/Datahub.cs
+++ Assets/Script/Datahub.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Threading;
+using Unity.Collections;
 using UnityEngine;
-// Span<int> sliceX = x.AsSpan().Slice(int,int); // 創建一個切片來訪問X座標的一部分
+
 public class Datahub 
 {
-    public const float squareSizew = 1.1f;  //寬度
-    public const float squareSizeh = 1.1f;  //高度
-    public const int z = 10; // 相機高度
-    static public readonly int[] x = new int[10000]; // 10000个格子，每个格子一个x坐标
-    static public readonly int[] y = new int[10000]; // 10000个格子，每个格子一个y坐标
-   
+    public struct Data
+    {
+        public int id; //細胞ID          4byte
 
-    static public int[] mouseX = new int[1]; // 鼠标点击位置的X坐标
-    static public int[] mouseY = new int[1]; // 鼠标点击位置的Y坐标
-    
+    }
 
-
-
+    public NativeArray<int> iscell;//宣告細胞陣列
+    public void coordinate(int size)
+    { 
+        iscell = new NativeArray<int>(size, Allocator.Persistent); //宣告細胞陣列大小 不回收
+    }
 }
-
-
-                    
--- Assets/Script/Gamemanager.cs
+++ Assets/Script/Gamemanager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Square : MonoBehaviour
+public class Gamemanager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
